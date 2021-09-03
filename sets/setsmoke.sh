2385951d148b12edf3f5ebedc21dac897501efeb
jiseong <cjs970220@gmail.com>
2021-09-03 18:48:59 +0900

    last raspberry update

--- sets/setsmoke.sh
+++ sets/setsmoke.sh
@@ -11,10 +11,10 @@ version="1.0 8/20/2021"
 export DEVICEID="smoke"
 
 #####DB CONFIG######
-export dbHost="158.247.214.81"
+export dbHost="121.157.22.62"
 export dbPort=3306
-export dbUser="root"
-export dbPwd="pass4sdms"
+export dbUser="smoke"
+export dbPwd="1q2w3e4r"
 export dbName="smoke"
 				
 alias gd="cd /home/smoke/data"
